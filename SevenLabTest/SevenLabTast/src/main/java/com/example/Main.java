ce28b3d8468a7e7861f4ce2a435a538540bdf238
axltsikh <lioshatichon@gmail.com>
2022-11-19 12:38:39 +0300

    asd

--- SevenLabTest/SevenLabTast/src/main/java/com/example/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Calculator.operation(5,5,'+');
-        Triangle.checkTriangle(15,15,15);
-        System.out.println("Hello world!");
-    }
-}
--- SevenLabTest/SevenLabTast/src/main/java/com/example/Triangle.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Triangle {
-    public static boolean checkTriangle(double first,double second,double third){
-        return first+second>third && first+third>second && second+third>first;
-    }
-
-}
