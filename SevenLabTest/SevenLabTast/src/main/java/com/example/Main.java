220a26e5777e6afcae33e511a81b7b6d900e5f28
axltsikh <lioshatichon@gmail.com>
2022-11-19 10:48:29 +0300

    m

---
+++ SevenLabTest/SevenLabTast/src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator.operation(5,5,'+');
+        Triangle.checkTriangle(15,15,15);
+        System.out.println("Hello world!");
+    }
+}
